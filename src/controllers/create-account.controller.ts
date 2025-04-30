716222d88e38eef96b5fe33ab0cd8877a3f40511
Eduardo Zago <zagoe22@gmail.com>
2025-04-30 11:56:49 -0300

    feat: add CreateAccountController to handle account creation

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,32 @@
+import { Body, ConflictException, Controller, Post } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+  @Post()
+  async handle(@Body() body: any) {
+    const { name, email, password } = body
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException()
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
