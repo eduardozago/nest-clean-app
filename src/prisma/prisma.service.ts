c189a3e2c6d4a4dfbca6521fc3b47c2c36785650
Eduardo Zago <zagoe22@gmail.com>
2025-05-02 17:42:22 -0300

    fix: change Prisma version to 6.5.0 to fix Prisma generate

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { OnModuleDestroy, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from 'generated/prisma'
+import { PrismaClient } from '@prisma/client'
 
 export class PrismaService
   extends PrismaClient
