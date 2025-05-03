a8fe80042d33587393347b2cee4149229f37f218
Eduardo Zago <zagoe22@gmail.com>
2025-05-03 16:22:55 -0300

    refactor: restructure application to separate infra layer

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -6,9 +6,9 @@ import {
   UsePipes,
 } from '@nestjs/common'
 import { z } from 'zod'
-import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { JwtService } from '@nestjs/jwt'
-import { PrismaService } from '@/prisma/prisma.service'
+import { PrismaService } from '@/infra/prisma/prisma.service'
 import { compare } from 'bcryptjs'
 
 const authenticateBodySchema = z.object({
