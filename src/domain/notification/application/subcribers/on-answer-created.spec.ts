8d0b1ed3bd4c5e4984f6b1d51a7698001f627e73
Eduardo Zago <zagoe22@gmail.com>
2025-05-03 14:19:06 -0300

    feat: add  forum domain entities and notification system from  clean-ddd-node repository

---
+++ src/domain/notification/application/subcribers/on-answer-created.spec.ts
@@ -0,0 +1,56 @@
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { OnAnswerCreated } from './on-answer-created'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
+import { makeQuestion } from 'test/factories/make-question'
+import { waitFor } from 'test/utils/wait-for'
+
+let inMemoryQuestionsAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sendNotificationUseCase: SendNotificationUseCase
+
+let sendNotificationExecuteSpy
+
+describe('On Answer Created', () => {
+  beforeEach(() => {
+    inMemoryQuestionsAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionsAttachmentsRepository,
+    )
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sendNotificationUseCase = new SendNotificationUseCase(
+      inMemoryNotificationsRepository,
+    )
+
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'execute')
+
+    new OnAnswerCreated(inMemoryQuestionsRepository, sendNotificationUseCase)
+  })
+
+  it('should send a notification when an answer is created', async () => {
+    const question = makeQuestion()
+
+    const answer = makeAnswer({ questionId: question.id })
+
+    inMemoryQuestionsRepository.create(question)
+    inMemoryAnswersRepository.create(answer)
+
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
+  })
+})
---
+++ src/domain/notification/application/subcribers/on-question-best-answer-chosen.ts
@@ -0,0 +1,39 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import { EventHandler } from '@/core/events/event-handler'
+import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { QuestionBestAnswerChosenEvent } from '@/domain/forum/enterprise/events/question-best-answer-chosen-event'
+
+export class OnQuestionBestAnswersChosen implements EventHandler {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendQuestionBestAnswerNotification.bind(this),
+      QuestionBestAnswerChosenEvent.name,
+    )
+  }
+
+  private async sendQuestionBestAnswerNotification({
+    question,
+    bestAnswerId,
+  }: QuestionBestAnswerChosenEvent) {
+    const answer = await this.answersRepository.findById(
+      bestAnswerId.toString(),
+    )
+
+    if (answer) {
+      await this.sendNotification.execute({
+        recipientId: answer.authorId.toString(),
+        title: 'Your answers was chosen!',
+        content: `The answer that you sent on "${question.title.substring(0, 20).concat('...')}" was chosen by the author!`,
+      })
+    }
+  }
+}
