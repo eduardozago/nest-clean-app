145fcafe876cd64845f374406624c1430c6aa9a1
Eduardo Zago <zagoe22@gmail.com>
2025-05-13 13:14:45 -0300

    refactor: move database folder to infra

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,7 +3,7 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { DatabaseModule } from './database/database.module'
+import { DatabaseModule } from '../database/database.module'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
