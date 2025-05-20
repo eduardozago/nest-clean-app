c9518c11b9b7680c94948e7be29697dfccdcc332
Eduardo Zago <zagoe22@gmail.com>
2025-05-20 13:48:14 -0300

    feat: create delete answer comment controller and test

---
+++ src/infra/http/controllers/delete-answer-comment.controller.ts
@@ -0,0 +1,46 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  ForbiddenException,
+  HttpCode,
+  NotFoundException,
+  Param,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
+
+@Controller('/answers/comments/:id')
+export class DeleteAnswerCommentController {
+  constructor(private deleteAnswerComment: DeleteAnswerCommentUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') answerCommentId: string,
+  ) {
+    const userId = user.sub
+
+    const result = await this.deleteAnswerComment.execute({
+      authorId: userId,
+      answerCommentId,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case ResourceNotFoundError:
+          throw new NotFoundException(error.message)
+        case NotAllowedError:
+          throw new ForbiddenException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+  }
+}
