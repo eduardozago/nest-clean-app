bf948c0e16bc962d88da4a0fa08388e376e2687c
Eduardo Zago <zagoe22@gmail.com>
2025-05-26 17:32:39 -0300

    feat: create make attachment factory for e2e test

---
+++ test/factories/make-attachment.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
+import {
+  Attachment,
+  AttachmentProps,
+} from '@/domain/forum/enterprise/entities/attachment'
+import { PrismaAttachmentsMapper } from '@/infra/database/prisma/mappers/prisma-attachment-mapper'
+
+export function makeAttachment(
+  override: Partial<AttachmentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const attachment = Attachment.create(
+    {
+      title: faker.lorem.sentence(),
+      url: faker.internet.url(),
+      ...override,
+    },
+    id,
+  )
+
+  return attachment
+}
+
+@Injectable()
+export class AttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAttachment(
+    data: Partial<AttachmentProps> = {},
+  ): Promise<Attachment> {
+    const attachment = makeAttachment(data)
+
+    await this.prisma.attachment.create({
+      data: PrismaAttachmentsMapper.toPrisma(attachment),
+    })
+
+    return attachment
+  }
+}
