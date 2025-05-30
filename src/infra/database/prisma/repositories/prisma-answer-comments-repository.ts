56808597fa1a16e0466141f74a948739ec34a56f
Eduardo Zago <zagoe22@gmail.com>
2025-05-30 15:12:39 -0300

    feat: implement find many by answer id with author on answer comments repository

--- src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-comments-repository.ts
@@ -4,6 +4,8 @@ import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment
 import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaAnswerCommentMapper } from '../mappers/prisma-answer-comment-mapper'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
 
 @Injectable()
 export class PrismaAnswerCommentsRepository
@@ -40,7 +42,32 @@ export class PrismaAnswerCommentsRepository
       skip: (page - 1) * 20,
     })
 
-    return comments.map(PrismaAnswerCommentMapper.toDomain)
+    return comments.map((comment) => {
+      return PrismaAnswerCommentMapper.toDomain(comment)
+    })
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const comments = await this.prisma.comment.findMany({
+      where: {
+        answerId,
+      },
+      include: {
+        author: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return comments.map((comment) => {
+      return PrismaCommentWithAuthorMapper.toDomain(comment)
+    })
   }
 
   async create(answerComment: AnswerComment): Promise<void> {
