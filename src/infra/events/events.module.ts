d39febc42d65685081ed66e1bff498d0058b77fc
Eduardo Zago <zagoe22@gmail.com>
2025-06-01 17:28:58 -0300

    feat: create prisma notifications repository and mapper

--- src/infra/events/events.module.ts
+++ src/infra/events/events.module.ts
@@ -1,8 +1,8 @@
 import { OnAnswerCreated } from '@/domain/notification/application/subcribers/on-answer-created'
 import { OnQuestionBestAnswersChosen } from '@/domain/notification/application/subcribers/on-question-best-answer-chosen'
 import { SendNotificationUseCase } from '@/domain/notification/application/use-cases/send-notification'
-import { DatabaseModule } from '@faker-js/faker/.'
 import { Module } from '@nestjs/common'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
   imports: [DatabaseModule],
